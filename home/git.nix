105c0994e07f9dd2a355964dd04444d0cdeddabd
Miles Lee <miles.lee@kupotech.com>
2026-05-07 12:10:40 +0800

    chore: update git config

--- home/git.nix
+++ home/git.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name = "Miles";
-      user.email = "mileslee1215@gmail.com";
-    };
+  programs.git.enable = true;
+
+  programs.git.config = {
+    init.defaultBranch = "master";
+    user.name = "Miles";
+    user.email = "mileslee1215@gmail.com";
+    core.editor = "vim";
+    pull.ff = "only";
   };
 }
